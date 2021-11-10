a13dd8cadba3cece0818cb70a45ea4bcf8069bbd
Roman Volozhanin <rsvolozhanin@gmail.com>
2021-11-10 08:59:24 +0300

    Move allowed_hosts to .env

--- project/settings.py
+++ project/settings.py
@@ -10,13 +10,13 @@ DATABASES = {'default': dj_database_url.config(default=env.str('DB_URL'))}
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = env.bool('SECRET_KEY', False)
+SECRET_KEY = env.str('SECRET_KEY', False)
 
 DEBUG = env.bool('DEBUG', False)
 
-ROOT_URLCONF = "project.urls"
+ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
